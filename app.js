2f114c868a1948620b7d671cd63dfc1a9581b9c7
Hary Nugraha Putra <harynugrahaputra@gmail.com>
2017-12-07 11:14:08 +0700

    Fix Error Backend

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ app.use(bodyParser.json())
 app.use(cors())
 
 var mongoose = require('mongoose');
-mongoose.connect('mongodb://harynp:harynp@pratice-shard-00-00-vajls.mongodb.net:27017,pratice-shard-00-01-vajls.mongodb.net:27017,pratice-shard-00-02-vajls.mongodb.net:27017/livecoding?ssl=true&replicaSet=Pratice-shard-0&authSource=admin', (err) => {
+mongoose.connect('mongodb://hary:hary@cluster0-shard-00-00-dvvn1.mongodb.net:27017,cluster0-shard-00-01-dvvn1.mongodb.net:27017,cluster0-shard-00-02-dvvn1.mongodb.net:27017/test?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin', (err) => {
   err ? console.log('Can\'t connect to database') : console.log('Database connected')
 });
 
