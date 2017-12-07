6169c863813b0974297b6a42ba76393537c6ed01
Hary Nugraha Putra <harynugrahaputra@gmail.com>
2017-12-07 10:08:17 +0700

    Done Server

--- app.js
+++ app.js
@@ -1,15 +1,24 @@
 const express = require('express');
-const app = express;
+const bodyParser = require('body-parser')
+const cors = require('cors')
+const app = express()
+
+app.use(bodyParser.urlencoded({ extended: false }))
+
+app.use(bodyParser.json())
+app.use(cors())
 
 var mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost/api-crud-mongoose', (err) => {
+mongoose.connect('mongodb://harynp:harynp@pratice-shard-00-00-vajls.mongodb.net:27017,pratice-shard-00-01-vajls.mongodb.net:27017,pratice-shard-00-02-vajls.mongodb.net:27017/livecoding?ssl=true&replicaSet=Pratice-shard-0&authSource=admin', (err) => {
   err ? console.log('Can\'t connect to database') : console.log('Database connected')
 });
 
-var books = require('routes/books');
-var transactions = require('routes/transactions');
+var books = require('./routes/books');
+var transactions = require('./routes/transactions');
 
 app.use('/books', books);
 app.use('/transactions', transactions);
 
-app.listen(3000)
+app.listen(3000, function () {
+  console.log('SERVER JALAN');
+})
