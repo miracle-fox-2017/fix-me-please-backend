e966f662d5828fc9cd9bd482dddf22e302c33e15
AhmadNizar <ahmadnizar.owl@gmail.com>
2017-12-07 10:13:24 +0700

    -fix book controller

--- controllers/books.js
+++ controllers/books.js
@@ -1,37 +1,40 @@
-module.exports = {
-  all: function(req, res) {
-    Book.find(function (err, books) {
-      if (err) {
-        res.send({err: err})
-      }
-      res.send(books)
-    })
-  },
-  create: function(req, res) {
-    var book = new Book(req.body);
-    book.save(function (err, result) {
-      if (err) {
-        res.send({err: err})
-      }
-      res.send(result)
-    });
-  },
-  update: function(req, res) {
-    Book.update({ _id: req.id }, {
-      $set: req.body
-    }, function(err, result) {
-      if (err) {
-        res.send({err: err})
-      }
-      res.send(result)
-    });
-  },
-  delete: function(req, res) {
-    Book.remove({ _id: req.id }, function (err, result) {
-      if (err) {
-        res.send({err: err})
-      }
-      res.send(result)
-    });
-  }
+const Book = require("../models/Book")
+
+class BookCtrl {
+
+	static all (req, res) {
+		Book.find()
+			.then(books => res.status(200).send(books))
+			.catch(err => res.status(500).send(err))  
+	}
+
+	static create (req, res) {
+		var book = new Book(req.body)
+		book.save()
+			.then(result => res.status(200).send(result))
+			.catch(err => res.status(500).send(err))
+	}
+	
+	static update (req, res) {
+		Book.update({ _id: req.id }, {
+			$set: req.body
+		}, function(err, result) {
+			if (err) {
+				res.status(500).send(err)
+			}
+			res.status(500).send(result)
+		})
+	}
+
+	static delete (req, res) {
+		Book.remove({ _id: req.id }, function (err, result) {
+			if (err) {
+				res.status(500).send(err)
+			}
+			res.status(200).send(result)
+		})
+	}
 }
+
+
+module.exports = BookCtrl
