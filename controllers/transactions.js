894d4c280f98c8f1b9f93bd69476e68cc6995d62
Tomy Djaya Budiman <tommybudi1998@gmail.com>
2017-12-07 10:31:05 +0700

    Create & Get Transaction done

--- controllers/transactions.js
+++ controllers/transactions.js
@@ -1,3 +1,6 @@
+const ObjectId = require('mongoose').Types.ObjectId;
+const Transaction = require('../models/Transaction');
+
 module.exports = {
   all: function(req, res) {
     Transaction.find(function (err, transactions) {
@@ -5,17 +8,22 @@ module.exports = {
         res.send({err: err})
       }
       res.send(transactions)
-    })
+    }).populate('booklist')
   },
   create: function(req, res) {
-    var transaction = new Transaction(req.body);
+    var transaction = new Transaction({
+      memberid:req.body.memberid,
+      days:req.body.days,
+      date:req.body.date,
+      price:req.body.price,
+      booklist:req.body.booklist.split(',')
+    });
     transaction.save(function (err, result) {
       if (err) {
         res.send({err: err})
       } else {
         res.send(result)
       }
-      res.send(result)
     });
   },
   update: function(req, res) {
