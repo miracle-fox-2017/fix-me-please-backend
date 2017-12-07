bb2943f526a00bdc8623eead7e9bd0589d6eb32d
azhariemuhammad <azharie_muhammad@live.com>
2017-12-07 10:38:56 +0700

    connect ke database works

--- app.js
+++ app.js
@@ -1,5 +1,10 @@
 const express = require('express');
-const app = express;
+const app = express()
+const bodyParser = require('body-parser')
+
+app.use(bodyParser.urlencoded({ extended: false }))
+// parse application/json
+app.use(bodyParser.json())
 
 var mongoose = require('mongoose');
 mongoose.connect('mongodb://localhost/api-crud-mongoose', (err) => {
