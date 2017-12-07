2d7b152ead248a384a5fcd725106abd37b9f31dc
Zuhri Nurhuda <zuhri.nurhuda@gmail.com>
2017-12-07 10:26:28 +0700

    fixing book controller for delete

--- controllers/books.js
+++ controllers/books.js
@@ -21,13 +21,9 @@ module.exports = {
     });
   },
   update: function(req, res) {
-    console.log(req.params);
-    console.log(req.body);
-
     let id = { _id: ObjectId(req.params.id)}
     Book.findById(id)
     .then(book => {
-      console.log(book);
       book.isbn = req.body.isbn || book.isbn,
       book.title = req.body.title || book.title,
       book.author = req.body.author || book.author,
@@ -35,23 +31,23 @@ module.exports = {
       book.stock = req.body.stock || book.stock
 
       book.save()
-      .then(result => {
-        console.log(result)
-        res.send(result)
-      })
-      .catch(err => res.send(err))
+      .then(result => res.send(result))
+      .catch(err => console.log(err))
     })
-    // Book.findById({ _id: req.id }, {
-    //   $set: {req.body}
-    // }, function(err, result) {
-    //   if (err) {
-    //     res.send({err: err})
-    //   }
-    //   res.send(result)
-    // });
+    .catch(err => console.log(err))
+    /* Code lama
+    Book.findById({ _id: req.params.id }, {
+      $set: {req.body}
+    }, function(err, result) {
+      if (err) {
+        res.send({err: err})
+      }
+      res.send(result)
+    });
+    */
   },
   delete: function(req, res) {
-    Book.remove({ _id: req.id }, function (err, result) {
+    Book.remove({ _id: req.params.id }, function (err, result) {
       if (err) {
         res.send({err: err})
       }
