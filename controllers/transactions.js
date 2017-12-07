72d21b5281a4e23c69b31a8bc54270b60218ba49
ameliarahman <amel.rahman5@gmail.com>
2017-12-07 10:05:02 +0700

    Missing ) and typo create

--- controllers/transactions.js
+++ controllers/transactions.js
@@ -1,4 +1,5 @@
 module.exports = {
+
   all: function(req, res) {
     Transaction.find(function (err, transactions) {
       if (err) {
@@ -7,7 +8,8 @@ module.exports = {
       res.send(transactions)
     })
   },
-  craete: function(req, res) {
+
+  create: function(req, res) {
     var transaction = new Transaction(req.body);
     transaction.save(function (err, result) {
       if (err) {
@@ -18,6 +20,7 @@ module.exports = {
       res.send(result)
     });
   },
+
   update: function(req, res) {
     Transaction.update({ _id: req.id }, {
       $set: req.body
@@ -28,12 +31,15 @@ module.exports = {
       res.send(result)
     });
   },
+
   delete: function(req, res) {
-    Transaction.remove({ _id: req.id }, function (err, result) {
+    Transaction.remove(
+      { _id: req.id }, function (err, result) {
       if (err) {
         res.send({err: err})
       }
       res.send(result)
-    }
-  });
+    })
+  }
+
 }
