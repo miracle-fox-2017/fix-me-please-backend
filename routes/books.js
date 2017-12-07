7f904d1fd48b2801f01b5b8d714630cc118aea31
albertagung <albiardtya@gmail.com>
2017-12-07 10:06:21 +0700

    error typo sudah hilang

--- routes/books.js
+++ routes/books.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const router = express.Router;
+const router = express.Router();
 var booksController = require('../controllers/books');
 
 router.get('/', booksController.all)
