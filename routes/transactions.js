a1544f9d5e9ed0da0122a767539039794a683fe7
setiadijoe <setiadi_y56@yahoo.co.id>
2017-12-07 09:59:04 +0700

    resolve name of function

--- routes/transactions.js
+++ routes/transactions.js
@@ -2,7 +2,9 @@ const express = require('express');
 const router = express.Router()
 var transactionController = require('../controllers/transactions');
 
-router.get('/', transactionsController.all)
-router.post('/', transactionsController.create)
-router.put('/:id', transactionsController.update)
-router.delete('/:id', transactionsController.delete)
+router.get('/', transactionController.all)
+router.post('/', transactionController.create)
+router.put('/:id', transactionController.update)
+router.delete('/:id', transactionController.delete)
+
+module.exports = router;
