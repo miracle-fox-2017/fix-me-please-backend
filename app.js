f4f482e06a49382a151216c2528ec1afefdf69eb
aditReza <lete.note3@gmail.com>
2017-12-07 10:53:57 +0700

    mongoose fix

--- app.js
+++ app.js
@@ -7,9 +7,9 @@ mongoose.connect('mongodb://localhost/api-crud-mongoose', (err) => {
 });
 
 var books = require('./routes/books');
-var transactions = require('routes/transactions');
+// var transactions = require('./routes/transactions');
 
 app.use('/books', books);
-app.use('/transactions', transactions);
+// app.use('/transactions', transactions);
 
 app.listen(3000)
