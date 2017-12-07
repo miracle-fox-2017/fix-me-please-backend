2f78afac03c3495e3445851e7ef314e9fc7cf702
Faris Widyantho <radenfaris@gmail.com>
2017-12-07 11:26:53 +0700

    fix post backend

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-
+var bodyParser = require('body-parser')
 var mongoose = require('mongoose');
 mongoose.connect('mongodb://localhost:27017/api-crud-mongoose', (err) => {
   err ? console.log('Can\'t connect to database') : console.log('Database connected')
@@ -8,6 +8,9 @@ mongoose.connect('mongodb://localhost:27017/api-crud-mongoose', (err) => {
 
 var books = require('./routes/books');
 var transactions = require('./routes/transactions');
+
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
 
 app.use('/books', books);
 app.use('/transactions', transactions);
