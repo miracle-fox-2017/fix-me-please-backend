e6569334190afb2a97be4a64fb71a426bfcb7fdb
Zuhri Nurhuda <zuhri.nurhuda@gmail.com>
2017-12-07 10:22:01 +0700

    fixing book controller for update

--- controllers/books.js
+++ controllers/books.js
@@ -1,4 +1,5 @@
 const Book = require('../models/Book')
+const ObjectId = require('mongodb').ObjectId
 
 module.exports = {
   all: function(req, res) {
@@ -10,7 +11,7 @@ module.exports = {
     })
   },
   create: function(req, res) {
-    console.log(req.body);
+    // console.log(req.body);
     var book = new Book(req.body);
     book.save(function (err, result) {
       if (err) {
@@ -20,14 +21,34 @@ module.exports = {
     });
   },
   update: function(req, res) {
-    Book.update({ _id: req.id }, {
-      $set: req.body
-    }, function(err, result) {
-      if (err) {
-        res.send({err: err})
-      }
-      res.send(result)
-    });
+    console.log(req.params);
+    console.log(req.body);
+
+    let id = { _id: ObjectId(req.params.id)}
+    Book.findById(id)
+    .then(book => {
+      console.log(book);
+      book.isbn = req.body.isbn || book.isbn,
+      book.title = req.body.title || book.title,
+      book.author = req.body.author || book.author,
+      book.category = req.body.category || book.category,
+      book.stock = req.body.stock || book.stock
+
+      book.save()
+      .then(result => {
+        console.log(result)
+        res.send(result)
+      })
+      .catch(err => res.send(err))
+    })
+    // Book.findById({ _id: req.id }, {
+    //   $set: {req.body}
+    // }, function(err, result) {
+    //   if (err) {
+    //     res.send({err: err})
+    //   }
+    //   res.send(result)
+    // });
   },
   delete: function(req, res) {
     Book.remove({ _id: req.id }, function (err, result) {
